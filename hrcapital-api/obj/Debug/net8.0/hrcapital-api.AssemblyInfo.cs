c340e14dda3b06d358174c50e66228a0474b3745
Harshad Rane <harshadrane67@gmail.com>
2025-06-29 18:58:39 +0000

    HR-003 : Added docker file

--- hrcapital-api/obj/Debug/net8.0/hrcapital-api.AssemblyInfo.cs
+++ hrcapital-api/obj/Debug/net8.0/hrcapital-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hrcapital-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f27adbe3d6a66fa80cc9d349f32694401c9ee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4805378686aad91b2ec3cde431ca8b85f5829e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("hrcapital-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hrcapital-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
